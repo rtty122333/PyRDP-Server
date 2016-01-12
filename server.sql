a55fc6479ce24e5d29ac52dedb2f9958e8d72ba0
bafangchaoshi <fangyafenqidai@163.com>
2016-01-12 21:56:03 +0800

    mysql data update

--- server.sql
+++ server.sql
@@ -18,12 +18,12 @@ INSERT INTO pyrdpserver.user ( userName, password ,roleId,state,stateTime,create
 	##1-已分配，未连接，userId:vm所属的用户, stateTime:分配虚拟机的时间
 	##2-已分配，正在连接，userId:vm所属的用户, stateTime:连接虚拟机的时间
 	##3-已分配，断开连接，userId:vm所属的用户, stateTime:断开连接虚拟机的时间
-	##4-取消分配，入待重新分配，userId:vm最后一次所属的用户, stateTime:取消虚拟机所属用户的时间
+	##4-取消分配，入待回收站等待删除，userId:vm最后一次所属的用户, stateTime:取消虚拟机所属用户的时间
 CREATE TABLE pyrdpserver.vm(id  INT PRIMARY KEY auto_increment, vmId varchar(32), userName varchar(32) ,vmName varchar(32),ip varchar(32), password varchar(32),state INT, stateTime varchar(32),ownerName varchar(32),createTime varchar(32), lastModifyTime varchar(32),unique(vmId)) default charset=utf8;
-INSERT INTO pyrdpserver.vm(vmId ,userName,vmName, ip,password,state,stateTime,ownerName,createTime , lastModifyTime) VALUES('1', 'vm1','vm1','192.168.1.107','134650',1,1403507414000.0,'user','1403507414000.0','1403507414000.0');
-INSERT INTO pyrdpserver.vm(vmId ,userName,vmName, ip,password,state,stateTime,ownerName,createTime , lastModifyTime) VALUES('2', 'vm2','vm2','192.168.1.107','134650',1,1403507414000.0,'user','1403507414000.0','1403507414000.0');
-INSERT INTO pyrdpserver.vm(vmId ,userName,vmName, ip,password,state,stateTime,ownerName,createTime , lastModifyTime) VALUES('3', 'vm3','vm3','192.168.1.107','134650',0,1403507414000.0,NULL,'1403507414000.0','1403507414000.0');
-INSERT INTO pyrdpserver.vm(vmId ,userName,vmName, ip,password,state,stateTime,ownerName,createTime , lastModifyTime) VALUES('4', 'vm4','vm4','192.168.1.107','134650',1,1403507414000.0,'user2','1403507414000.0','1403507414000.0');
+INSERT INTO pyrdpserver.vm(vmId ,userName,vmName, ip,password,state,stateTime,ownerName,createTime , lastModifyTime) VALUES('1', 'fyf','vm1','100.100.100.108','134650',1,1403507414000.0,'user','1403507414000.0','1403507414000.0');
+INSERT INTO pyrdpserver.vm(vmId ,userName,vmName, ip,password,state,stateTime,ownerName,createTime , lastModifyTime) VALUES('2', 'fyf','vm2','192.168.1.107','134650',1,1403507414000.0,'user','1403507414000.0','1403507414000.0');
+INSERT INTO pyrdpserver.vm(vmId ,userName,vmName, ip,password,state,stateTime,ownerName,createTime , lastModifyTime) VALUES('3', 'fyf','vm3','192.168.1.107','134650',0,1403507414000.0,NULL,'1403507414000.0','1403507414000.0');
+INSERT INTO pyrdpserver.vm(vmId ,userName,vmName, ip,password,state,stateTime,ownerName,createTime , lastModifyTime) VALUES('4', 'fyf','vm4','192.168.1.107','134650',1,1403507414000.0,'user2','1403507414000.0','1403507414000.0');
 
 
 alter table pyrdpserver.role add index(roleName);
